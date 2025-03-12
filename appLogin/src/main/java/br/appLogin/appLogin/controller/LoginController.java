d48e7652f3f88fc1be885c8a28dfa673783cf097
edugitqa <eduardo.telecomrbs@gmail.com>
2025-03-12 01:44:18 -0300

    banco de dados validacao

--- appLogin/src/main/java/br/appLogin/appLogin/controller/LoginController.java
+++ appLogin/src/main/java/br/appLogin/appLogin/controller/LoginController.java
@@ -1,13 +1,44 @@
 package br.appLogin.appLogin.controller;
 
+import br.appLogin.appLogin.model.Usuario;
+import br.appLogin.appLogin.repository.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-    @GetMapping ("/login")
+    @Autowired
+    private UsuarioRepository ur;
+
+    @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/cadastroUsuario")
+    public String cadastro() {
+        return "cadastro";
+    }
+
+
+    @RequestMapping(value = "/cadastroUsuario", method = RequestMethod.POST)
+    public String cadastroUsuario(@Valid Usuario usuario, @NotNull BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "redirect:/cadastroUsuario";
+        }
+
+        ur.save(usuario);
+        System.out.println("Usuário cadastrado com sucesso!");
+        System.out.println("Redirecionando para /login...");
+        return "redirect:/login";
     }
 }
